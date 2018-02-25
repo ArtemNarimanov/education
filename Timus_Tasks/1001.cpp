e0f2bab850b7607e2ba640f9154291724656f72a
ArtemNarimanov <36588652+ArtemNarimanov@users.noreply.github.com>
2018-02-25 15:41:13 +0300

    Update 1001.cpp

--- Timus_Tasks/1001.cpp
+++ Timus_Tasks/1001.cpp
@@ -7,7 +7,7 @@ int main()
     int i = 0;
     unsigned long long n; 
     while (scanf("%llu", &n) != EOF) {
-        buf[i ++] = (double)sqrt(n * 1.0);
+        v[i ++] = (double)sqrt(n * 1.0);
     }
     for (; --i >= 0; ) {
         printf("%.4lf\n", v[i]); 
