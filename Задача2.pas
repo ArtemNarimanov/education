111bd719d53cf13b1ee81ec3285e5637aaed4001
ArtemNarimanov <36588652+ArtemNarimanov@users.noreply.github.com>
2018-09-21 22:45:51 +0300

    Create Задача2.pas

---
+++ Задача2.pas
@@ -0,0 +1,20 @@
+uses crt;
+const
+  z = 15.001;
+  k = 8;
+var
+  u, r: integer;
+  x, y, a, b: Real;
+begin
+ write('x->');
+readln(x);
+write('y->');
+readln(y);
+  u:= 8 * 2 div 3 * 2 - 15;
+  r := 7 - 10 mod 3 * 2;
+  a := (U + 5 * X) / (R / z - 31) + (1 - z) / Y;
+  b := z * (X / (0.4 - k) + Y / U / (25 * k));
+  WriteLn('a = ', A:4:2);
+  WriteLn('b = ', B:4:2);
+
+end.
